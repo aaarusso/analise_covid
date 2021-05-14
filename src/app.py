0ec6bc30c764d5060e5d3fb4e4e9af862942e4ae
Alexandre <aaarusso@gmail.com>
2021-05-14 17:15:55 -0300

    atualizacao para deploy Streamlit

--- src/app.py
+++ src/app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import pandas as pd
+import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
 
@@ -20,19 +21,27 @@ def grafico_comparativo(dados_2019, dados_2020, causa, estado="BRASIL"):
     dados = pd.DataFrame({"Total": lista, "Ano": [2019, 2020]})
 
     #plt.figure(figsize=(6,4))
-    return sns.barplot(x="Ano", y="Total", data = dados)
-    #plt.title(f"Óbitos por {causa} em {estado}")
-    #plt.show()
+    fig, ax = plt.subplots()
+    ax = sns.barplot(x="Ano", y="Total", data = dados)
+    ax.set_title(f"Óbitos por {causa} em {estado}")
+
+    return fig
 
 def main():
 
     obitos_2019 = carrega_dados("dados\obitos-2019.csv")   
     obitos_2020 = carrega_dados("dados\obitos-2020.csv")   
-
-    figura = grafico_comparativo(obitos_2019, obitos_2020, "SRAG")
+    
+    tipo_doenca = obitos_2020["tipo_doenca"].unique()
+    estado = np.append(obitos_2020["uf"].unique(), "BRASIL")
 
     st.title("Análise de óbitos 2019-2020")
     st.markdown("Este trabalho analisa dados dos **óbitos 2019-2020**")
+
+    opcao_1 = st.sidebar.selectbox("Selecione o tipo de doença", tipo_doenca)
+    opcao_2 = st.sidebar.selectbox("Selecione o Estado", estado)
+
+    figura = grafico_comparativo(obitos_2019, obitos_2020, opcao_1, opcao_2)
 
     st.pyplot(figura)
 
